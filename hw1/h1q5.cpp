013529c7850e797e0be766b7cf9a0a1be07aa278
Miles Phan <milespha@usc.edu>
2018-08-25 23:05:31 -0700

    Added h1q5.cpp

---
+++ hw1/h1q5.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <sstream>
+#include <cstring> 
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+	if(argc < 2){
+		cout << "Please input a file name to read" << endl;
+		return 1;
+	}
+	ifstream ifile (argv[1]);
+	if (ifile.fail()){
+		cout << "Couldn't open file" << endl;
+		return 1;
+	}
+	int size = 0;
+	ifile >> size;
+	string* words = new string[size];
+	string buff = "";
+	for (int i = 0; i < size; i++){
+		ifile >> buff;
+		words[size - i - 1] = buff;
+	}
+	for (int i = 0; i < size; i++){
+		cout << words[i] << endl;
+	}
+	ifile.close();
+	delete [] words;
+	return 0;
+}

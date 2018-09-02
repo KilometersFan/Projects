aac5763825e6547e04a52cce288a13db34f697d7
Miles Phan <milespha@usc.edu>
2018-09-01 21:47:49 -0700

    renamed simplestmemmanager to flexmemmanager in secondmemtest.cpp

--- hw1/second_memtest.cpp
+++ hw1/second_memtest.cpp
@@ -10,13 +10,13 @@ void flexCharManager::print_buff(){
 }
 int main(int argc, char *argv[])
 {
-  flexCharManager simplest_mem_manager;
-  char* c1 = simplest_mem_manager.flex_alloc_chars(3);
+  flexCharManager flex_mem_manager;
+  char* c1 = flex_mem_manager.flex_alloc_chars(3);
   c1[0] = 'i';
   c1[1] = 'n';
   c1[2] = ' ';
-  simplest_mem_manager.print_buff();
-  char* c2 = simplest_mem_manager.flex_alloc_chars(7);
+  flex_mem_manager.print_buff();
+  char* c2 = flex_mem_manager.flex_alloc_chars(7);
   c2[0] = 'F';
   c2[1] = 'r';
   c2[2] = 'e';
@@ -24,8 +24,8 @@ int main(int argc, char *argv[])
   c2[4] = 'c';
   c2[5] = 'h';
   c2[6] = ' ';
-  simplest_mem_manager.print_buff();
-  char* c3 = simplest_mem_manager.flex_alloc_chars(7);
+  flex_mem_manager.print_buff();
+  char* c3 = flex_mem_manager.flex_alloc_chars(7);
   c3[0] = 'c';
   c3[1] = 'h';
   c3[2] = 'a';
@@ -33,17 +33,17 @@ int main(int argc, char *argv[])
   c3[4] = 'e';
   c3[5] = 'a';
   c3[6] = 'u';
-  simplest_mem_manager.print_buff();
-  simplest_mem_manager.flex_free_chars(c1);
-  simplest_mem_manager.print_buff();
-  char* c4 = simplest_mem_manager.flex_alloc_chars(3);
+  flex_mem_manager.print_buff();
+  flex_mem_manager.flex_free_chars(c1);
+  flex_mem_manager.print_buff();
+  char* c4 = flex_mem_manager.flex_alloc_chars(3);
   c4[0] = 't';
   c4[1] = 'o';
   c4[2] = 'p';
-  simplest_mem_manager.print_buff();
-  simplest_mem_manager.flex_free_chars(c2);
-  simplest_mem_manager.print_buff();
-  char* c5 = simplest_mem_manager.flex_alloc_chars(8);
+  flex_mem_manager.print_buff();
+  flex_mem_manager.flex_free_chars(c2);
+  flex_mem_manager.print_buff();
+  char* c5 = flex_mem_manager.flex_alloc_chars(8);
   c5[0] = 's';
   c5[1] = 'o';
   c5[2] = 'm';
@@ -52,11 +52,11 @@ int main(int argc, char *argv[])
   c5[5] = 'e';
   c5[6] = 'r';
   c5[7] = 'o';
-  simplest_mem_manager.print_buff();
-  char* c6 = simplest_mem_manager.flex_alloc_chars(3);
+  flex_mem_manager.print_buff();
+  char* c6 = flex_mem_manager.flex_alloc_chars(3);
   c6[0] = 'h';
   c6[1] = 'a';
   c6[2] = 't';  
-  simplest_mem_manager.print_buff();
+  flex_mem_manager.print_buff();
   return 0;
 }

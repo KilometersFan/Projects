5edee45ddf9bfad15b501a61c666fd93b620388a
Miles Phan <milespha@usc.edu>
2018-10-16 20:10:03 -0700

    added mergeSort split logic

--- hw5/msort.h
+++ hw5/msort.h
@@ -1,8 +1,9 @@
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
-template <class T, class Comparator>
-std::vector<std::pair<int,int>> findIndices (std::vector<T> &myArray, int k, Comparator comp){
+template <class T>
+std::vector<std::pair<int,int>> findIndices(std::vector<T> &myArray, int k){
 	std::vector<std::pair<int, int>> indices;
 	int range = myArray.size()-1;
 	int i = 0;
@@ -20,19 +21,45 @@ std::vector<std::pair<int,int>> findIndices (std::vector<T> &myArray, int k, Com
 }
 
 template <class T, class Comparator>
+void merge (std::vector<T>& myArray, std::vector<std::vector<int>> &ranges, int k, Comparator comp){
+	int minIndex = 0;
+	int vectorCount = 0;
+	for(unsigned int i = 0; i < ranges.size(); i++){
+		std::cout << myArray[minIndex] << " " << myArray[ranges[i][0]] << std::endl;
+		T lhs = myArray[minIndex];
+		T rhs = myArray[ranges[i][0]];
+		if(!ranges[i].empty()){
+			if(!comp(lhs, rhs))
+			{
+				vectorCount = i;
+				minIndex = ranges[i][0];
+			}
+
+		}
+	}
+	// std::swap(myArray[minIndex], myArray[0]);
+	// ranges[vectorCount].erase(ranges[vectorCount].begin());
+}
+
+template <class T, class Comparator>
 void mergeSort (std::vector<T>& myArray, int k, Comparator comp){
 	if(myArray.size() <= 1)
 		return;
-	std::vector<std::pair<int,int>> indices = findIndices(myArray, k, comp);
-	for(int i = 0; i < indices.size(); i++){
-		std::vector<T> kthArray;
-		for(int j = indices[i].first; j <= indices[i].second; j++)
-			kthArray.push_back(myArray[j]);
-		for(int k = 0; k < kthArray.size(); k++)
-			std::cout << kthArray[k] << std::endl;
-		std::cout << "index " << indices[i].first << std::endl; 
-		mergeSort(kthArray, k, comp);
-
+	std::vector<std::pair<int,int>> indices = findIndices(myArray, k);
+	std::vector<std::vector<int>> kvectors;
+	for(unsigned int j = 0; j < indices.size(); j++){
+		std::vector<int> kindices;
+		for(int i = indices[j].first; i <= indices[j].second; i++){
+			kindices.push_back(i);
+		}
+		for(unsigned int m = 0; m < kindices.size(); m++)
+			std::cout << kindices[m] << std::endl;
+		std::cout << std::endl;
+		kvectors.push_back(kindices);
 	}
+	std::cout << "indices pushed" << std::endl;
+	for(unsigned int l = 0; l < kvectors.size(); l++)
+		mergeSort(kvectors[l], k , comp);
+	merge(myArray, kvectors, k, comp);
 
 }
--- hw5/test.cpp
+++ hw5/test.cpp
@@ -14,10 +14,9 @@ int main(int argc, char const *argv[])
 	strings.push_back("hi");
 	strings.push_back("hey");
 	strings.push_back("hello");
-	strings.push_back("hu");
-	strings.push_back("ho");
-	strings.push_back("hi");
 	NumStrComp comp1;
-	mergeSort(strings, 2, comp1);
+	mergeSort(strings, 3, comp1);
+	// for (int i = 0; i < strings.size(); i++)
+	// 	cout << strings[i] << endl;
 	return 0;
 }
